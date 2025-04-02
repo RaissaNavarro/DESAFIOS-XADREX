ff35cb4f40f0dd5e8afc61581848cc2ceeeb60dd
Raissa Navarro <144500102+RaissaNavarro@users.noreply.github.com>
2025-04-02 01:33:48 +0000

    codigo completo

---
+++ NivelNovato/nivelNovato.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main () {
+
+    int i = 0;
+    int cont = 0;
+
+    printf("\n*=====================*\n");
+    printf("Movimento da TORRE:\n");
+    for (int i = 0; i < 5; i++){
+        printf("Direita.\n");
+    }
+    printf("*=====================*\n");
+
+    printf("\n*=====================*");
+    printf("Movimento do BISPO\n");
+
+    while (i <= 4 ){
+        printf("Cima, Direita.\n");
+        i++;
+    }
+    printf("*=====================*\n");
+
+    printf("\n*=====================*\n");
+    printf("Movimento da RAINHA\n");
+
+    do
+    { 
+        printf("Esquerda.\n"); 
+        cont++; 
+        
+    } while (cont < 8);
+
+
+    printf("*=====================*\n");
+    
+}
