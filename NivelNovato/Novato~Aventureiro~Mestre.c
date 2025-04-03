cd1fc44e47bd5b07d2c9351b82b6b8612b9b325c
Raissa Navarro <144500102+RaissaNavarro@users.noreply.github.com>
2025-04-03 00:12:16 +0000

    ajustes no código para o desafio aventureiro.

---
+++ NivelNovato/Novato~Aventureiro~Mestre.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int main () {
+
+    int i = 0;
+    int cont = 0;
+
+    printf("\n*=====================*\n");
+    printf("Movimento da TORRE:\n");
+    for (int i = 0; i < 5; i++){
+        printf("Direita.\n");
+    }
+    printf("*=====================*\n");
+
+    printf("\n*=====================*");
+    printf("Movimento do BISPO\n");
+
+    while (i <= 4 ){
+        printf("Cima, Direita.\n");
+        i++;
+    }
+    printf("*=====================*\n");
+
+    printf("\n*=====================*\n");
+    printf("Movimento da RAINHA\n");
+
+    do
+    { 
+        printf("Esquerda.\n"); 
+        cont++; 
+        
+    } while (cont < 8);
+
+    printf("*=====================*\n");
+
+
+    printf("\n*=====================*\n");
+    printf("Movimento do CAVALO\n");
+    for (int j = 1; j <= 2; j++) {
+        printf("Baixo\n");
+    }
+    int k = 1;
+    while (k <= 1) {
+        printf("Esquerda\n");
+        k++;
+    }
+    printf("*=====================*\n");
+    
+
+}
